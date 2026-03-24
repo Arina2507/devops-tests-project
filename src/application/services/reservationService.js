7afa2fd58e2378ed0e182cd0e077f3bd32f4e124
Arina <nazhmedenova2020@gmail.com>
2026-03-24 22:21:52 +0100

    feat: prevent past reservations

--- src/application/services/reservationService.js
+++ src/application/services/reservationService.js
@@ -6,8 +6,15 @@ class ReservationService {
     this.now = now || (() => new Date());
   }
 
-  async createReservation() {
-    throw new Error("Not implemented");
+  async createReservation(input) {
+    const startAt = new Date(input.startAt);
+    const currentTime = this.now();
+
+    if (startAt < currentTime) {
+      throw new Error("Start time is in the past");
+    }
+
+    return input;
   }
 }
 
